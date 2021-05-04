9fa4f9870a32667dcd1bb47b31b197132ea758bd
devarrowsmith <devarrowsmith@gmail.com>
2021-05-04 20:49:24 +0100

    Refactor fizzBuzz function.

--- src/kata1.fizzBuzz.js
+++ src/kata1.fizzBuzz.js
@@ -2,13 +2,13 @@ const fizzBuzz = number => {
     if(!(number%3) && !(number%5)) {
         return "FizzBuzz";
     }
-    else if (!(number%3)) {
+    if (!(number%3)) {
         return "Fizz";
-    } else if (!(number%5)) {
+    }
+    if (!(number%5)) {
         return "Buzz";
-    } else {
-        return number;
-    };
+    }
+    return number;    
 };
 
 module.exports = fizzBuzz;
