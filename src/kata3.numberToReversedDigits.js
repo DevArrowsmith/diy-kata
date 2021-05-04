9f0eba7f7561325a6978178667e85f4f372eacbe
devarrowsmith <devarrowsmith@gmail.com>
2021-05-04 21:50:31 +0100

    Update numberToReversedDigits function to pass third test.

--- src/kata3.numberToReversedDigits.js
+++ src/kata3.numberToReversedDigits.js
@@ -1,5 +1,5 @@
 const numberToReversedDigits = number => {
-    if (typeof(number) === "number") {
+    if (typeof(number) === "number" && !isNaN(number)) {
         return number.toString().split("").reverse().map(numeral => Number(numeral));
     } else return "Non-number value entered. Please enter a number."
 };
