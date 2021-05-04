9a660985d3077200f5ed39ea52b7a49623a4e026
devarrowsmith <devarrowsmith@gmail.com>
2021-05-04 21:30:50 +0100

    Create test for numberToreversedDigits function: returns an array containing the reversed digits of a passed number.

--- test/kata3.numberToReversedDigits.test.js
+++ test/kata3.numberToReversedDigits.test.js
@@ -1,5 +1,8 @@
 const { numberToReversedDigits } = require("../src");
 
 describe("numberToReversedDigits", () => {
-  test("returns a reversed array of the number's digits", () => {});
+  test("returns a reversed array of the number's digits", () => {
+    expect(numberToReversedDigits(3692)).toStrictEqual([2, 9, 6, 3]);
+    expect(numberToReversedDigits(12345)).toStrictEqual([5, 4, 3, 2, 1]);
+  });
 });
