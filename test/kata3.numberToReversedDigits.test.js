3556b7cfe291c6c4dd6f17e5d9a1888c6ac947c2
devarrowsmith <devarrowsmith@gmail.com>
2021-05-04 21:42:01 +0100

    Create test for numberToreversedDigits function: returns an error message when a non-number value is passed in.

--- test/kata3.numberToReversedDigits.test.js
+++ test/kata3.numberToReversedDigits.test.js
@@ -5,4 +5,9 @@ describe("numberToReversedDigits", () => {
     expect(numberToReversedDigits(3692)).toStrictEqual([2, 9, 6, 3]);
     expect(numberToReversedDigits(12345)).toStrictEqual([5, 4, 3, 2, 1]);
   });
+  test("return an error if a non-number value is passed in", () => {
+    expect(numberToReversedDigits(true)).toBe("Non-number value entered. Please enter a number.");
+    expect(numberToReversedDigits([1, 2, 3])).toBe("Non-number value entered. Please enter a number.");
+    expect(numberToReversedDigits("Coffee")).toBe("Non-number value entered. Please enter a number.");
+  });
 });
