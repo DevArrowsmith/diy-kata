a06dcfa5532fb1933e1c655eede29adee8a33026
devarrowsmith <devarrowsmith@gmail.com>
2021-05-05 20:18:31 +0100

    Create humanCatDogYears test: returns an array.

--- test/kata4.humanCatDogYears.test.js
+++ test/kata4.humanCatDogYears.test.js
@@ -1,3 +1,9 @@
+const { TestScheduler } = require("jest");
 const { humanCatDogYears } = require("../src");
 
-// Look Ma, no handlebars!!!
+
+describe ("humanCatDogYears", () => {
+    test("returns an array", () => {
+        expect(Array.isArray(humanCatDogYears(1))).toBeTruthy();
+    })
+});
