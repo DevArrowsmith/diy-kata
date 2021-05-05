6e1fdc0c35872655f3b36bdb530fb7e722546f25
devarrowsmith <devarrowsmith@gmail.com>
2021-05-05 21:04:14 +0100

    Update humanCatDogYears function to pass fifth test.

--- src/kata4.humanCatDogYears.js
+++ src/kata4.humanCatDogYears.js
@@ -1,5 +1,21 @@
 const humanCatDogYears = number => {
-    return [1, 2, 3];
+    const ages = [number];
+    let catYears = 0;
+    
+    for (let i = 0; i < number + 1; i++) {
+        if (i === 1) {
+            catYears += 15;
+        };
+        if (i === 2) {
+            catYears += 9;
+        };
+        if (i > 2) {
+            catYears += 4;
+        };
+    }
+
+    ages.push(catYears, 1);
+    return ages;
 };
 
 module.exports = humanCatDogYears;
