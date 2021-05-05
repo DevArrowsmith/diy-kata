b0d51730dcd0c6689f3e357018ba07841290ae70
devarrowsmith <devarrowsmith@gmail.com>
2021-05-05 20:21:56 +0100

    Create humanCatDogYears test: returns an array containing 3 elements.

--- test/kata4.humanCatDogYears.test.js
+++ test/kata4.humanCatDogYears.test.js
@@ -4,4 +4,7 @@ describe ("humanCatDogYears", () => {
     test("returns an array", () => {
         expect(Array.isArray(humanCatDogYears(1))).toBeTruthy();
     });
+    test("returns an array containing 3 elements", () => {
+        expect(humanCatDogYears(1).length).toEqual(3);
+    });
 });
