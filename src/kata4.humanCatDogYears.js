dc07ad3d2874ee544aeaffa555712b250330b14a
devarrowsmith <devarrowsmith@gmail.com>
2021-05-05 21:36:33 +0100

    Update humanCatDogYears function to pass tenth test.

--- src/kata4.humanCatDogYears.js
+++ src/kata4.humanCatDogYears.js
@@ -12,7 +12,7 @@ const humanCatDogYears = number => {
     let catYears = 0;
     let dogYears = 0;
     
-    for (let i = 0; i < number + 1; i++) {
+    for (let i = 0; i < Math.floor(number) + 1; i++) {
         if (i === 1) {
             catYears += 15;
             dogYears += 15;
