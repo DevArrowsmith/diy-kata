ed957fab61c6ac11d8629d8e14da0b834a6facc0
devarrowsmith <devarrowsmith@gmail.com>
2021-05-05 20:32:11 +0100

    Create humanCatDogYears test: all elements in the array are numbers.

--- test/kata4.humanCatDogYears.test.js
+++ test/kata4.humanCatDogYears.test.js
@@ -5,6 +5,9 @@ describe ("humanCatDogYears", () => {
         expect(Array.isArray(humanCatDogYears(1))).toBeTruthy();
     });
     test("returns an array containing 3 elements", () => {
-        expect(humanCatDogYears(1).length).toEqual(3);
+        expect(humanCatDogYears(1).length).toBe(3);
+    });
+    test("all elements in the array are numbers", () => {
+        expect(humanCatDogYears(1).map(e => typeof(e))).toStrictEqual(["number", "number", "number"]);
     });
 });
