42cdfa5a99d809fb901a259a38069e7ae81f3d3d
devarrowsmith <devarrowsmith@gmail.com>
2021-05-05 21:15:22 +0100

    Update humanCatDogYears function to pass seventh test.

--- src/kata4.humanCatDogYears.js
+++ src/kata4.humanCatDogYears.js
@@ -1,4 +1,9 @@
 const humanCatDogYears = number => {
+
+    if (typeof(number) !== "number") {
+        return "Non-number value entered. Please enter a number."
+    };
+
     const ages = [number];
     let catYears = 0;
     let dogYears = 0;
@@ -16,7 +21,6 @@ const humanCatDogYears = number => {
             catYears += 4;
             dogYears += 5;
         };
-        console.log(catYears);
     }
 
     ages.push(catYears, dogYears);
