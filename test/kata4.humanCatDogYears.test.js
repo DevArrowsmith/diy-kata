3cd1f99487d4e5dd8914fbc8a35479031f22b324
devarrowsmith <devarrowsmith@gmail.com>
2021-05-05 20:39:29 +0100

    Create humanCatDogYears test: all elements in the array are not NaN.

--- test/kata4.humanCatDogYears.test.js
+++ test/kata4.humanCatDogYears.test.js
@@ -10,4 +10,7 @@ describe ("humanCatDogYears", () => {
     test("all elements in the array are numbers", () => {
         expect(humanCatDogYears(1).map(e => typeof(e))).toStrictEqual(["number", "number", "number"]);
     });
+    test("all elements in the array are not NaN", () => {
+        expect(humanCatDogYears(1).map(e => Number.isNaN(e))).toStrictEqual([false, false, false]);
+    });
 });
