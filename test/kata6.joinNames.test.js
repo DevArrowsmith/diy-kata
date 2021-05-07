f6801c1fa08a9de95b8cd062ea9951ce020f87c1
devarrowsmith <devarrowsmith@gmail.com>
2021-05-07 22:32:00 +0100

    Create  joinNames test: returns string of names, seperated by commas and an ampersand.

--- test/kata6.joinNames.test.js
+++ test/kata6.joinNames.test.js
@@ -4,6 +4,8 @@ const { simpsons, willows, witches } = require("../data/kata6.joinNamesData");
 
 describe("joinNames", () => {
   test("returns string of names, seperated by commas and an ampersand", () => {
-    
+    expect(joinNames(simpsons)).toBe("Bart, Lisa & Maggie.");
+    expect(joinNames(willows)).toBe("Mole, Rat, Toad & Badger.");
+    expect(joinNames(witches)).toBe("Weatherwax, Ogg, Garlick, Nitt, Tick, Treason, Level & Aching.");
   });
 });
