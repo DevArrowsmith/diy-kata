a16173008785cd0bf911a1e4872d61d8f1db2f10
devarrowsmith <devarrowsmith@gmail.com>
2021-05-07 21:40:03 +0100

    Add an additional expect block to the reachDestination test 'returns string with estimated time of arrival'.

--- test/kata5.reachDestination.test.js
+++ test/kata5.reachDestination.test.js
@@ -3,5 +3,6 @@ const { reachDestination } = require("../src");
 describe("reachDestination", () => {
   test("returns string with estimated time of arrival", () => {
     expect(reachDestination(15, 10)).toBe("I should be there in 1.5 hours.");
+    expect(reachDestination(271.22, 80)).toBe("I should be there in 3.5 hours.");
   });
 });
