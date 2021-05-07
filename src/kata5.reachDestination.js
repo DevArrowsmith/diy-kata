9cdca5888d78f9a212e3425d196c41a4dcd7e9c8
devarrowsmith <devarrowsmith@gmail.com>
2021-05-07 21:46:49 +0100

    Update reachDestination function to pass the test 'returns the string with singular 'hour' when arrival will be in one hour'.

--- src/kata5.reachDestination.js
+++ src/kata5.reachDestination.js
@@ -1,6 +1,9 @@
 const reachDestination = (distance, speed) => {
     const arrivalTime = Math.round(2 * distance / speed)/2;
-    return `I should be there in ${arrivalTime} hours.`
+
+    return arrivalTime === 1 
+    ? `I should be there in ${arrivalTime} hour.`
+    : `I should be there in ${arrivalTime} hours.`
 };
 
 module.exports = reachDestination;
