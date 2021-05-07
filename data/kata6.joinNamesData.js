4927bf9292f74e621c20ae2eb9c8ae75df40d66b
devarrowsmith <devarrowsmith@gmail.com>
2021-05-07 22:23:14 +0100

    Create mock data for the joinNames function.

---
+++ data/kata6.joinNamesData.js
@@ -0,0 +1,25 @@
+const simpsons = [
+    {name: 'Bart'},
+    {name: 'Lisa'},
+    {name: 'Maggie'}
+];
+
+const willows = [
+    {name: 'Mole'},
+    {name: 'Rat'},
+    {name: 'Toad'},
+    {name: 'Badger'},
+];
+
+const witches = [
+    {name: 'Weatherwax'},
+    {name: 'Ogg'},
+    {name: 'Garlick'},
+    {name: 'Nitt'},
+    {name: 'Tick'},
+    {name: 'Treason'},
+    {name: 'Level'},
+    {name: 'Aching'},
+];
+
+module.exports = { simpsons, willows, witches };
