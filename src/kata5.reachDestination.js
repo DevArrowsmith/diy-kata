c0f92f1d00a21705fd7674adcb360542c96be199
devarrowsmith <devarrowsmith@gmail.com>
2021-05-07 21:39:13 +0100

    Update reachDestination function to pass the test 'returns string with estimated time of arrival'.

--- src/kata5.reachDestination.js
+++ src/kata5.reachDestination.js
@@ -1,3 +1,6 @@
-const reachDestination = (distance, speed) => {};
+const reachDestination = (distance, speed) => {
+    const arrivalTime = Math.round(2 * distance / speed)/2;
+    return `I should be there in ${arrivalTime} hours.`
+};
 
 module.exports = reachDestination;
