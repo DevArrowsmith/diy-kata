a9ce15fe7cc8f0f91b3529faeb01071e4ae45075
devarrowsmith <devarrowsmith@gmail.com>
2021-05-07 22:46:57 +0100

    Create getEmployerRole test: returns the employee's role in the company.

--- test/kata7.getEmployerRole.test.js
+++ test/kata7.getEmployerRole.test.js
@@ -3,6 +3,8 @@ const { employees } = require("../data/kata7.getEmployerRoleData");
 
 describe("getEmployerRole", () => {
   test("returns the employee's role in the company", () => {
-    console.log(employees);
+    expect(getEmployerRole('Satti')).toBe("Developer");
+    expect(getEmployerRole('Jenny')).toBe("Sales Associate");
+    expect(getEmployerRole('Javid')).toBe("Human Recommended Reading Assistant");
   });
 });
