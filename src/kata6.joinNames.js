f4ba6a9627605b246d09ce71c0303226ea5588bb
devarrowsmith <devarrowsmith@gmail.com>
2021-05-07 22:37:03 +0100

    Update joinNames function to pass the test 'returns string of names, seperated by commas and an ampersand'.

--- src/kata6.joinNames.js
+++ src/kata6.joinNames.js
@@ -1,6 +1,9 @@
 const joinNames = namesObj => {
-
-    
+    const namesArray = namesObj.map(nameObj => {
+        return nameObj.name;
+    })
+    const finalName = namesArray.pop();
+    return namesArray.join(", ") + ` & ${finalName}.`;
 };
 
 module.exports = joinNames;
