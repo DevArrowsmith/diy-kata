dcfddd26f1e92c1f69198051333391c98a05aa92
devarrowsmith <devarrowsmith@gmail.com>
2021-05-07 22:50:43 +0100

    Amend getEmployerRole test: returns the employee's role in the company.

--- test/kata7.getEmployerRole.test.js
+++ test/kata7.getEmployerRole.test.js
@@ -3,8 +3,8 @@ const { employees } = require("../data/kata7.getEmployerRoleData");
 
 describe("getEmployerRole", () => {
   test("returns the employee's role in the company", () => {
-    expect(getEmployerRole('Satti')).toBe("Developer");
-    expect(getEmployerRole('Jenny')).toBe("Sales Associate");
-    expect(getEmployerRole('Javid')).toBe("Human Recommended Reading Assistant");
+    expect(getEmployerRole('Satti', employees)).toBe("Developer");
+    expect(getEmployerRole('Jenny', employees)).toBe("Sales Associate");
+    expect(getEmployerRole('Javid', employees)).toBe("Human Recommended Reading Assistant");
   });
 });
