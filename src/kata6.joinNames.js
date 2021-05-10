6cc85011639ed09c8172f9988eaaf2e8e0ab35eb
devarrowsmith <devarrowsmith@gmail.com>
2021-05-10 16:37:50 +0100

    Update joinNames function to pass the test 'returns a string consisting of a single name when the argument is an array of one name'.

--- src/kata6.joinNames.js
+++ src/kata6.joinNames.js
@@ -1,4 +1,9 @@
 const joinNames = namesObj => {
+
+    if (namesObj.length === 1) {
+        return `${namesObj[0].name}.`
+    };
+
     const namesArray = namesObj.map(nameObj => {
         return nameObj.name;
     })
